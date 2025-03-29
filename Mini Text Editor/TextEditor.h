88dfce18c118445b029874cfdb60a791e4273783
Tarek Abo Elenen <tarek.abo.elenen15@gmail.com>
2025-03-29 06:49:52 +0200

    implement add line function

---
+++ Mini Text Editor/TextEditor.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<iostream>
+#include<map>
+using namespace std;
+class TextEditor
+{
+public:
+	
+    void addLine(string& text);
+    void insertLine(int lineNumber, string& text);
+    string getLine(int lineNumber);
+    void deleteLine(int lineNumber);
+    void deleteRange(int startNumber, int endNumber);
+    void updateLine(int lineNumber, string& text);
+    map<int, string> findAll(string& search);
+    void findAndReplaceAll(string& oldString, string& newString);
+    void show();
+    void undo();
+
+private:
+    map<int, string> lines;
+    int count = 0;
+};
+
